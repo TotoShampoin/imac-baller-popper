73d4163b47364405aac22dbfd8d601ee7c28a184
TotoShampoin <thomasgarnier2010@hotmail.fr>
2022-12-17 13:05:25 +0100

    Partie 2: Afficher et déplacer une balle

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "displayator.h"
-#include "ball.h"
+#include "ball->h"
 
 #include "mymath.h"
 #include <ctime>
@@ -19,40 +19,36 @@ void event(bool* cont) {
 }
 
 int main(int argc, const char* argv[]) {
+    // Variables
+    Displayator* disp;
+    bool cont;
+    Ball* ball;
+
     // Initialisation
-    auto disp = new Displayator { WIDTH , HEIGHT };
-    bool cont = true;
-
     srand(time(NULL));
-
-    int len = 16;
-    Ball* ball = new Ball[len];
-    for(int i = 0; i < len; i++) {
-        ball[i].position.x = randint(ball[i].radius, WIDTH - ball[i].radius);
-        ball[i].position.y = randint(ball[i].radius, HEIGHT - ball[i].radius);
-        ball[i].color = 0xFF000000 + randint(0x000000, 0xFFFFFF);
-    }
+    disp = new Displayator { WIDTH , HEIGHT };
+    cont = true;
+    ball = new Ball;
+    ball->position.x = randint(ball->radius, WIDTH - ball->radius);
+    ball->position.y = randint(ball->radius, HEIGHT - ball->radius);
+    ball->color = 0xFF000000 + randint(0x000000, 0xFFFFFF);
 
     while(cont) {
         // Input
         event(&cont);
 
         // Physics
-        for(int i = 0; i < len; i++) {
-            ball[i].update();
-            ball[i].applyCollisionsAndBounce(WIDTH, HEIGHT);
-        }
+        ball->update();
+        ball->applyCollisionsAndBounce(WIDTH, HEIGHT);
 
         // Graphics
         disp->clear();
-        for(int i = 0; i < len; i++) {
-            disp->circle(
-                ball[i].position.x,
-                ball[i].position.y,
-                ball[i].radius,
-                ball[i].color
-            );
-        }
+        disp->circle(
+            ball->position.x,
+            ball->position.y,
+            ball->radius,
+            ball->color
+        );
         disp->refresh();
     }
     delete disp;
