61422653fe5ad57b891042f92ecebe1d4eb0a040
TotoShampoin <thomasgarnier2010@hotmail.fr>
2022-12-17 19:15:18 +0100

    Implémentation des listes chainées de balles

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "displayator.h"
 #include "ball.h"
+#include "linkedlistball.h"
 
 #include "mymath.h"
 #include <ctime>
@@ -18,39 +19,56 @@ void event(bool* cont) {
     }
 }
 
+void ballPhysics(Ball* ball, void* = nullptr) {
+    ball->update();
+    ball->applyCollisionsAndBounce(WIDTH, HEIGHT);
+}
+void ballGraphics(Ball* ball, void* _disp) {
+    Displayator* disp = (Displayator*)_disp;
+    disp->circle(
+        ball->position.x,
+        ball->position.y,
+        ball->radius,
+        ball->color
+    );
+}
+
 int main(int argc, const char* argv[]) {
     // Variables
     Displayator* disp;
     bool cont;
-    Ball* ball;
+    BallCollection* balls;
 
     // Initialisation
     srand(time(NULL));
     disp = new Displayator { WIDTH , HEIGHT };
     cont = true;
-    ball = new Ball;
-    ball->position.x = randint(ball->radius, WIDTH - ball->radius);
-    ball->position.y = randint(ball->radius, HEIGHT - ball->radius);
-    ball->color = 0xFF000000 + randint(0x000000, 0xFFFFFF);
+    balls = new BallCollection;
+    for(int i = 0; i < 16; i++) {
+        auto ball = new Ball;
+        ball->position.x = randint(ball->radius, WIDTH - ball->radius);
+        ball->position.y = randint(ball->radius, HEIGHT - ball->radius);
+        ball->color = 0xFF000000 + randint(0x000000, 0xFFFFFF);
+        balls->add(ball);
+    }
 
     while(cont) {
         // Input
         event(&cont);
 
         // Physics
-        ball->update();
-        ball->applyCollisionsAndBounce(WIDTH, HEIGHT);
+        balls->forEach(ballPhysics);
+        
 
         // Graphics
         disp->clear();
-        disp->circle(
-            ball->position.x,
-            ball->position.y,
-            ball->radius,
-            ball->color
-        );
+        balls->forEach(ballGraphics, disp);
         disp->refresh();
+
     }
+
+    // Delete
     delete disp;
+    delete balls;
     return 0;
 }
