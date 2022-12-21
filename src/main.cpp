3e871ca62bed73392399ac2d7d203819a692481c
TotoShampoin <thomasgarnier2010@hotmail.fr>
2022-12-21 19:26:31 +0100

    Added tiny gui

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,57 @@
-#include "displayator.h"
-#include "ball.h"
-#include "linkedlistball.h"
+#include "main.h"
 
-#include "mymath.h"
-#include <ctime>
-#include <iostream>
-using namespace std;
+int main(int argc, const char* argv[]) {
+    // Variables
+    bool cont;
+    Displayator* disp;
+    BallCollection* balls;
+    MouseData mouse;
+    int maxball;
 
-#define WIDTH 720
-#define HEIGHT 540
+    // Initialisation
+    srand(time(NULL));
+    disp = new Displayator { WIDTH , HEIGHT };
+    balls = new BallCollection;
+    maxball = (argc == 1) ? 16 : argc - 1;
+    for(int i = 0; i < maxball; i++) {
+        int color = (argc == 1) 
+            ? randint(0x000000, 0xFFFFFF) 
+            : std::stoi(argv[i + 1], 0, 16);
+        balls->add(new Ball(WIDTH, HEIGHT, color));
+    }
 
-struct MouseData {
-    Vec2 position;
-    bool click = false;
-    bool is_down = false;
-};
+    // Game loop
+    cont = true;
+    while(cont) {
+        // Input
+        event(&cont, &mouse);
+
+        // Physics
+        balls->forEach(ballPhysics);
+        if(mouse.click) {
+            BallCollection* removed = balls->filterOut(ballContact, (void*)&mouse.position);
+            if(removed->length == 0) {
+                int color = randint(0x000000, 0xFFFFFF);
+                balls->add(new Ball(WIDTH, HEIGHT, color));
+            }
+            delete removed;
+        }
+
+        // Graphics
+        disp->clear();
+        balls->forEach(ballGraphics, disp);
+        disp->text(16, 16, to_string(balls->length).c_str());
+        disp->refresh();
+
+        // SDL_Delay(1000/60);
+    }
+
+    // Delete
+    delete disp;
+    delete balls;
+    return 0;
+}
+
 
 void event(bool* cont, MouseData* mouse) {
     int mx, my, mc;
@@ -25,9 +62,10 @@ void event(bool* cont, MouseData* mouse) {
     case SDL_QUIT:
         *cont = false;
         break;
-    default: break;
+    default:
+        SDL_PumpEvents();
+        break;
     }
-    SDL_PumpEvents();
     mc = SDL_GetMouseState(&mx, &my);
     mouse->is_down = SDL_BUTTON(mc) == SDL_BUTTON_LEFT;
     if(mouse->is_down) {
@@ -39,69 +77,23 @@ void event(bool* cont, MouseData* mouse) {
     mouse->position.y = my;
 }
 
-void ballPhysics(Ball* ball, void* = nullptr) {
+void ballPhysics(Ball* ball, void*) {
     ball->update();
     ball->applyCollisionsAndBounce(WIDTH, HEIGHT);
 }
+
 void ballGraphics(Ball* ball, void* _disp) {
     Displayator* disp = (Displayator*)_disp;
     disp->circle(
         ball->position.x,
         ball->position.y,
         ball->radius,
-        ball->color,
-        ball->marked
+        ball->color
     );
 }
-int indx;
+
 bool ballContact(Ball* ball, void* _p) {
     Vec2 position = *(Vec2*)_p;
     bool res = !((ball->position - position).magnitude2() <= ball->radius * ball->radius);
     return res;
-}
-
-int main(int argc, const char* argv[]) {
-    // Variables
-    bool cont;
-    Displayator* disp;
-    BallCollection* balls;
-    MouseData mouse;
-
-    // Initialisation
-    srand(time(NULL));
-    disp = new Displayator { WIDTH , HEIGHT };
-    cont = true;
-    balls = new BallCollection;
-    for(int i = 0; i < 16; i++) {
-        balls->add(new Ball(WIDTH, HEIGHT));
-    }
-
-    while(cont) {
-        // Input
-        event(&cont, &mouse);
-
-        // Physics
-        balls->forEach(ballPhysics);
-        indx = 0;
-        if(mouse.click) {
-            BallCollection* removed = balls->filterOut(ballContact, (void*)&mouse.position);
-            if(removed->length == 0) {
-                balls->add(new Ball(WIDTH, HEIGHT));
-            }
-            delete removed;
-        }
-
-        // Graphics
-        disp->clear();
-        balls->forEach(ballGraphics, disp);
-        disp->circle(mouse.position.x, mouse.position.y, 3, 0xFFFFFFFF, false);
-        disp->refresh();
-
-        // SDL_Delay(1000/60);
-    }
-
-    // Delete
-    delete disp;
-    delete balls;
-    return 0;
 }
